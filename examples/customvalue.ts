28a94fb5dc8a03ac25f5fe9ab0fe64360ee0f856
Denis Carriere <carriere.denis@gmail.com>
2024-01-26 17:25:46 -0500

    update examples

--- examples/customvalue.ts
+++ examples/customvalue.ts
@@ -1,9 +1,9 @@
-import { ACCOUNT, rpc } from "../src/config.js";
+import { ACCOUNT, abi, rpc } from "../src/config.js";
 import { read_only, decode } from "../src/utils.js"
 
 const name = "customvalue";
 const data = { message: "hello", extra: "world", number: 123 };
 const { processed } = await read_only(rpc, ACCOUNT, name, data);
 const { return_value_hex_data } = processed.action_traces[0];
-const value = decode(return_value_hex_data, name);
-console.log({ name, data, return_value_hex_data, value });
+const value = decode(abi, return_value_hex_data, name);
+console.log({ account: ACCOUNT, name, data, return_value_hex_data, value });
--- examples/namevalue.ts
+++ examples/namevalue.ts
@@ -1,9 +1,9 @@
 import { read_only, decode } from "../src/utils.js"
-import { ACCOUNT, rpc } from "../src/config.js";
+import { ACCOUNT, abi, rpc } from "../src/config.js";
 
 const name = "namevalue";
 const data = { message: "foobar" };
 const { processed } = await read_only(rpc, ACCOUNT, name, data);
 const { return_value_hex_data } = processed.action_traces[0];
-const value = decode(return_value_hex_data, name);
-console.log({ name, data, return_value_hex_data, value });
+const value = decode(abi, return_value_hex_data, name);
+console.log({ account: ACCOUNT, name, data, return_value_hex_data, value });
